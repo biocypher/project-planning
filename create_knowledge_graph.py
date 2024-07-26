1e14bf55b805922a8e31ef38c4f63d65bb166112
slobentanzer <sebastian.lobentanzer@gmail.com>
2024-07-26 13:16:49 +0200

    make docker work, update biocypher

--- create_knowledge_graph.py
+++ create_knowledge_graph.py
@@ -11,14 +11,20 @@ def main():
 
     adapter = GitHubAdapter()
 
-    bc.add_nodes(adapter.get_nodes())
-    bc.add_edges(adapter.get_edges())
+    # bc.add_nodes(adapter.get_nodes())
+    # bc.add_edges(adapter.get_edges())
 
-    dfs = bc.to_df()
+    # dfs = bc.to_df()
 
-    for name, df in dfs.items():
-        print(name)
-        print(df.head())
+    # for name, df in dfs.items():
+    #     print(name)
+    #     print(df.head())
+
+    bc.write_nodes(adapter.get_nodes())
+    bc.write_edges(adapter.get_edges())
+    bc.write_schema_info(as_node=True)
+    bc.write_import_call()
+    bc.summary()
 
 
 if __name__ == "__main__":
